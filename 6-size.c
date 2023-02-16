331612b2ddbde352d90de4402c0d55eedcb7db34
Frank <franknuori@gmail.com>
2023-02-16 03:06:06 -0800

    5-printf.c

---
+++ 6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ * /
+int main(void)
+{
+printf("size of a char: %i byte(s)\n", sizeo f(char));
+printf("size of a char: %i byte(s)\n", sizeo f(int));
+printf("size of a char: %i byte(s)\n", sizeo f(long int));
+printf("size of a char: %i byte(s)\n", sizeo f(char));
+printf("size of a char: %i byte(s)\n", sizeo f(long long int));
+printf("size of a char: %i byte(s)\n", sizeo f(float));
+return (0);
+}
